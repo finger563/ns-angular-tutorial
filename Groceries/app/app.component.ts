5e22fc1fab33b290d6d4b95fa63b5f251b6acda8
William Emfinger <waemfinger@gmail.com>
2017-12-11 16:23:24 -0600

    login tutorial.

--- Groceries/app/app.component.ts
+++ Groceries/app/app.component.ts
@@ -2,6 +2,29 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  template: `
+    <StackLayout>
+      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+      <TextField hint="Email Address" keyboardType="email" [(ngModel)]="email"
+        autocorrect="false" autocapitalizationType="none"></TextField>
+      <TextField hint="Password" secure="true"></TextField>
+
+      <Button [text]="isLoggingIn ? 'Sign In' : 'Sign Up'" class="submit-button" (tap)="submit()"></Button>
+      <Button [text]="isLoggingIn ? 'Sign Up' : 'Back to Groceries'" (tap)="toggleDisplay()"></Button>
+    </StackLayout>
+  `,
+  styleUrls: [
+    "pages/login/login-common.css",
+    "pages/login/login.css"
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+  email = "nativescriptrocks@telerik.com";
+  isLoggingIn = true;
+  submit() {
+    alert("You're using: " + this.email);
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
